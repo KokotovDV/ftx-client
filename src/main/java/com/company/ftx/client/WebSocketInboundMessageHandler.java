39cbf0cf85467ee4175687eb2400e6145d368aa2
Dmitry Kokotov <KokotovDV@gmail.com>
2020-11-02 06:34:49 +0300

    ftx client implementation

--- src/main/java/com/company/ftx/client/WebSocketInboundMessageHandler.java
+++ src/main/java/com/company/ftx/client/WebSocketInboundMessageHandler.java
@@ -1,11 +1,10 @@
 package com.company.ftx.client;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.integration.handler.GenericHandler;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Dmitry Kokotov
@@ -14,17 +13,19 @@ import java.util.regex.Pattern;
 public class WebSocketInboundMessageHandler implements GenericHandler<String> {
     @Override
     public Object handle(String payload, MessageHeaders headers) {
-        Pattern pattern = Pattern.compile("(?=\"bid\": )(.*)(?=, \"ask\":)|(?= \"ask\": )(.*)(?=, \"bidSize\":)");
-        Matcher matcher = pattern.matcher(payload);
         StringBuilder stringBuilder = new StringBuilder();
-        while(matcher.find()) {
-            stringBuilder.append(payload, matcher.start(), matcher.end());
+        JsonObject jsonObject = JsonParser.parseString(payload).getAsJsonObject();
+        if (jsonObject.isJsonObject() && jsonObject.has("data")) {
+            JsonObject data = jsonObject.get("data").getAsJsonObject();
+            if (data.has("bid")  && data.has("ask")) {
+                stringBuilder.append("bid:");
+                stringBuilder.append(data.get("bid").getAsString());
+                stringBuilder.append(", ask:");
+                stringBuilder.append(data.get("ask").getAsString());
+                System.out.println(stringBuilder.toString());
+            }
         }
-        if (stringBuilder.length() != 0) {
-            System.out.println(stringBuilder.toString()
-                    .replace("\"","")
-                    .replace(": ", ":"));
-        } else {
+        if (stringBuilder.length() == 0) {
             System.out.println(payload);
         }
         return null;
